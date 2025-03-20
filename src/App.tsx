0f003209ccfa70363b024d2d31a49f30491f7ded
Obianuju Jonathan <ObianujuJonathan@gmail.com>
2025-03-20 09:06:11 +0100

    path

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/offshorebanking/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/verify-otp" element={<VerifyOtpPage />} />
       </Routes>
     </>
